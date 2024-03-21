8d15a9ca41845a5d3cf1607b128edf858d432281
Mohor Banerjee <115227638+MoBanerjee@users.noreply.github.com>
2024-03-21 21:48:09 +0800

    Update firebase.config.js

--- Lab 3 Deliverables/Application Skeleton/client/src/pages/firebase.config.js
+++ Lab 3 Deliverables/Application Skeleton/client/src/pages/firebase.config.js
@@ -7,13 +7,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCNillgfdot6q51Pjz_OGW1HaNxYm_2AUg",
-  authDomain: "my-first-proj-85a18.firebaseapp.com",
-  projectId: "my-first-proj-85a18",
-  storageBucket: "my-first-proj-85a18.appspot.com",
-  messagingSenderId: "35012588797",
-  appId: "1:35012588797:web:7ab4810643e28968c67a94",
-  measurementId: "G-YELP5NZ5HC"
+
 };
 
 // Initialize Firebase
