75e1a9e28ecd6137ea18d6f215cd47b26c9cb21e
Mohor Banerjee <115227638+MoBanerjee@users.noreply.github.com>
2024-04-07 02:16:56 +0800

    Update API_KEYS.jsx

--- Lab 4 Deliverables/Source Code/client/src/pages/API_KEYS.jsx
+++ Lab 4 Deliverables/Source Code/client/src/pages/API_KEYS.jsx
@@ -1,6 +1,6 @@
-const NEWS_API_KEY="9ac74291a9b946408e8247bf0bf7b03e";
-const X_RapidAPI_Key='8a772231aamshb51aad46f08dee9p1b3105jsn222a34c99ca5';
-const GOOGLE_MAPS_API_KEY='AIzaSyAHdDz4Q_16G8ipNhz1Fy9CU4x1uwkmw5k';
+const NEWS_API_KEY="";
+const X_RapidAPI_Key='';
+const GOOGLE_MAPS_API_KEY='';
 export const NEWS_AK=NEWS_API_KEY;
 export const X_RapidAPI_AK=X_RapidAPI_Key;
 export const GOOGLE_MAPS_AK=GOOGLE_MAPS_API_KEY;
