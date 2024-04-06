9fdb5551ae29dbe4e7ff3fe5743947056b746d76
Mohor Banerjee <115227638+MoBanerjee@users.noreply.github.com>
2024-04-07 02:17:45 +0800

    Update firebase.config.js

--- Lab 4 Deliverables/Source Code/client/src/pages/firebase.config.js
+++ Lab 4 Deliverables/Source Code/client/src/pages/firebase.config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNillgfdot6q51Pjz_OGW1HaNxYm_2AUg",
-  authDomain: "my-first-proj-85a18.firebaseapp.com",
-  projectId: "my-first-proj-85a18",
-  storageBucket: "my-first-proj-85a18.appspot.com",
-  messagingSenderId: "35012588797",
-  appId: "1:35012588797:web:7ab4810643e28968c67a94",
-  measurementId: "G-YELP5NZ5HC"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
